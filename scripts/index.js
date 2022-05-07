41a8ab2afdd14f221e507bea982b10e595200895
SmallPlayz <900raiyan@gmail.com>
2022-05-07 19:01:29 -0400

    added blur when you scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 200) {
+    document.getElementById("html").className = "ScrollBackgroundChange";
+    console.log("1");
+  } else {
+    document.getElementById("html").className = "";
+    console.log("0");
+  }
+}
