35d66f1b2e898def8766770ae13c126b47eb47c5
SmallPlayz <900raiyan@gmail.com>
2022-05-08 16:55:27 -0400

    put image before index

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,11 @@ console.log("Hello World");
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 200) {
-    document.getElementById("html").className = "ScrollBackgroundChange";
+  if (document.documentElement.scrollTop > 50) {
+    document.getElementById("top").className = "ScrollBackgroundChange";
     console.log("1");
   } else {
-    document.getElementById("html").className = "";
+    document.getElementById("top").className = "";
     console.log("0");
   }
 }
